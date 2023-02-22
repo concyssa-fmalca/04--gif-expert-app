64c640f9be16854eb73999d8f07ca9ec554572a1
fmalca <67565141+concyssa-fmalca@users.noreply.github.com>
2023-02-22 11:45:52 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import { AddCategory } from './components/AddCategory';
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']); 
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory         
+        setCategories={setCategories}           
+        />
+     
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+    </>
+  );
+}
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+export const AddCategory = ({setCategories}) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const onSubmit = (e)=>{
+        e.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        setCategories(categories => [inputValue,...categories])
+        setInputValue('');
+    }
+
+  return (
+    <form onSubmit={onSubmit}>
+        <input 
+            type="text"
+            placeholder="Buscar gifs"
+            value ={inputValue}
+            onChange = {onInputChange}
+        />
+
+    </form>
+  )
+}
